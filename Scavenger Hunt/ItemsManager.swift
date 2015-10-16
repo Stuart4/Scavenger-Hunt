6f7efdb8c8c121a3dad3eea2988561ed7c8e01b3
iOS App Challenge <iosappchallenge@icloud.com>
2015-10-15 23:04:53 -0400

    adds saving functionality

---
+++ Scavenger Hunt/ItemsManager.swift
@@ -0,0 +1,45 @@
+//
+//  ItemsManager.swift
+//  Scavenger Hunt
+//
+//  Created by Apple on 10/15/15.
+//  Copyright © 2015 Jacob. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ItemsManager {
+    var items = [ScavengerHuntItem]()
+    
+    func archivePath() -> String? {
+        let directoryList = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        if let documentsPath = directoryList.first {
+            return documentsPath + "/ScavengerHuntItems"
+        }
+        assertionFailure("Coult not determine where to save the file")
+        return nil
+    }
+    
+    func save() {
+        if let theArchivePath = archivePath() {
+            if NSKeyedArchiver.archiveRootObject(items, toFile: theArchivePath) {
+                print("Saved successfully")
+            } else {
+            assertionFailure("Could not save to \(theArchivePath)")
+            }
+        }
+    }
+    
+    func unarchiveSavedItems() {
+        if let theArchivePath = archivePath() {
+            if NSFileManager.defaultManager().fileExistsAtPath(theArchivePath) {
+                items = NSKeyedUnarchiver.unarchiveObjectWithFile(theArchivePath) as! [ScavengerHuntItem]
+            }
+        }
+    }
+    
+    init() {
+        unarchiveSavedItems()
+    }
+}
